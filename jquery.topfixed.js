99d53382b4e4fe2e198a9039fe57398333b93bdc
uehara-n <uehara-naf@UNIVERSE.LOCAL>
2018-08-28 18:35:29 +0900

    bk18:35

--- jquery.topfixed.js
+++ jquery.topfixed.js
@@ -1,19 +1,19 @@
-(function($){
-	var $win = $(windows),
-	var $main = $('#KeyVisual'),
-	var $nav = $('#head'),
-	var navHeight = $nav.outerHeight(),
-	var navPos = $nav.offset().top,
-	var fixedClass = 'is_fixed';
+$(function() {
+	var $win = $(window),
+			$main = $('.slicknav_menu'),
+			$nav = $('#header'),
+			navHeight = $nav.outerHeight(),
+			navPos = $nav.offset().top,
+			fixedClass = 'is-fixed';
 
-	$win.on('load scroll', function(){
+	$win.on('load scroll', function() {
 		var value = $(this).scrollTop();
-		if ( value > navPos ){
+		if ( value > navPos ) {
 			$nav.addClass(fixedClass);
-			$main.css('margin-top',navHeight);
-		}else{
+			$main.css('margin-top', navHeight);
+		} else {
 			$nav.removeClass(fixedClass);
-			$main.cdd('margin-top','0');
+			$main.css('margin-top', '0');
 		}
 	});
-})(jQuery);
+});
